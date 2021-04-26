0bb94426ee00a95c3e0ad8a4ba0d3ce6940907c3
Lydoche <lydietr27@gmail.com>
2021-04-26 19:54:41 +0200

    implement error popup

--- arnstruct/Gui/exemple1.py
+++ arnstruct/Gui/exemple1.py
@@ -5,6 +5,7 @@ from arnstruct.core.structure import Structure
 
 
 sg.theme('DarkTeal2')   # Add a touch of color
+
 # All the stuff inside your window.
 frame_seq1 = [
     [sg.Text('Importer un fichier'), sg.FileBrowse('Importer')],
@@ -18,7 +19,6 @@ frame_seq1 = [
 frame_seq2 = [
         [sg.Text('Importer un fichier'), sg.FileBrowse('Importer')],
         [sg.Text("Entrer manuellement sa séquence nucléotidique")], [sg.InputText(key='-SEQNT2-')],
-                [sg.Button('Valider', key='-SUB_SEQ2-')],
 
 
         [sg.Text("ou", justification='center')],
@@ -31,11 +31,19 @@ frame_motif = [
     [sg.Text('Quelle taille de motif voulez-vous ?'), sg.InputText(key='MOTIFSIZE')]
 ]
 
+frame_res = [
+    [sg.Text("Structure commune ")]
+]
 layout = [  [sg.Frame('ARN 1', frame_seq1), sg.VerticalSeparator(),sg.Frame('ARN 2', frame_seq2)],
-            [sg.Frame('Recherche de motifs communs', frame_motif)], [sg.Button('Comparer', key='-COMPARE-')]
+            [sg.Frame('Recherche de motifs communs', frame_motif)], [sg.Button('Comparer les motifs de séquence', key='-COMPAREMOTIF-')]
             ]
+
+layout_error = [
+    [sg.Text("Veuillez vérifier le format des entrées")]
+]
 # Create the Window
 window = sg.Window('Comparaison de deux séquences ARN',  layout, size=(710, 800))
+
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
@@ -44,27 +52,27 @@ while True:
         break
 
 
-    if event == '-COMPARE-':
-        seq1 = Sequence(values['-SEQNT1-'])
-        seq2 = Sequence(values['-SEQNT2-'])
-
-        struct1 = Structure(values["-STRUCT1-"])
-        struct2 = Structure(values["-STRUCT2-"])
-
+    if event == '-COMPAREMOTIF-':
 
         try:
+            seq1 = Sequence(values['-SEQNT1-'])
+            seq2 = Sequence(values['-SEQNT2-'])
+
+            struct1 = Structure(values["-STRUCT1-"])
+            struct2 = Structure(values["-STRUCT2-"])
+
             th_str = values['MOTIFSIZE']
             th_seq = int(th_str)
             motifs_seq = seq1.motif_search(seq2, th_seq)
-            print("Motifs communs de taille %d"%th_seq, motifs_seq)
+            print("Motifs communs de taille %d" % th_seq, motifs_seq)
 
             th_struct = int(th_str)
             motifs_struct = struct1.motif_search_struct(struct2, th_struct)
-            print("Structure commune de taille %d"%th_struct, motifs_struct)
+            print("Structure commune de taille %d" % th_struct, motifs_struct)
 
+        except ValueError:
 
-        except ValueError as e:
-            print(f"Nan mais on a dit une taille !")
+           sg.popup("Veuillez vérifier le format des entrées")
 
 
     if event == '-SUB_SEQ2-':
